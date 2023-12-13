7525cd87eb0442906a7b6d0fbe28b68a263cdc7b
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-13 23:56:17 +0300

    Update ContentView.swift

--- CryptoApplication/ContentView.swift
+++ CryptoApplication/ContentView.swift
@@ -10,26 +10,6 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.crp.background
-                .ignoresSafeArea()
-
-            VStack {
-                Text("Accent Color")
-                    .foregroundStyle(Color.crp.accent)
-                    .font(.headline)
-
-                Text("Secondary Text")
-                    .foregroundStyle(Color.crp.secondaryText)
-                    .font(.headline)
-
-                Text("Red Color")
-                    .foregroundStyle(Color.crp.red)
-                    .font(.headline)
-
-                Text("Green Color")
-                    .foregroundStyle(Color.crp.green)
-                    .font(.headline)
-            }
         }
     }
 }
