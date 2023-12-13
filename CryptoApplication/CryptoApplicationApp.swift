5149ce4c212471db1e4d6fea8e412198066efd88
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-13 23:57:07 +0300

    Update CryptoApplicationApp.swift

--- CryptoApplication/CryptoApplicationApp.swift
+++ CryptoApplication/CryptoApplicationApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptoApplicationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
