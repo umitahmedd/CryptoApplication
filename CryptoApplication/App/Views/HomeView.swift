4140d968c0c660d9dbf7f12a21b17f76c3d18da4
umitahmedd <135972028+umitahmedd@users.noreply.github.com>
2023-12-21 02:14:28 +0300

    Update HomeView

--- CryptoApplication/App/Views/HomeView.swift
+++ CryptoApplication/App/Views/HomeView.swift
@@ -8,100 +8,97 @@
 import SwiftUI
 
 struct HomeView: View {
-    @EnvironmentObject private var viewModel: HomeViewModel
-    @State private var showPortfolio: Bool = false
-    var body: some View {
-        ZStack {
-            // Background
-            Color.crp.background
-                .ignoresSafeArea()
+   @EnvironmentObject private var viewModel: HomeViewModel
+   @State private var showPortfolio: Bool = false
+   var body: some View {
+      ZStack {
+         // Background
+         Color.crp.background
+            .ignoresSafeArea()
 
-            // Content
-            VStack(alignment: .leading, spacing: nil) {
-                // Header
-                homeHeader
+         // Content
+         VStack(alignment: .leading, spacing: nil) {
+            // Header
+            homeHeader
 
-                HStack {
-                    Text("Coin")
+            HStack {
+               Text("Coin")
 
-                    Spacer()
+               Spacer()
 
-                    if showPortfolio {
-                        Text("Holdigns")
-                            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+               if showPortfolio {
+                  Text("Holdigns")
+                     .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
 
-                        Spacer()
-                    }
+                  Spacer()
+               }
 
-                    Text("Pirce")
-                }
-                .foregroundStyle(Color.crp.secondaryText)
-                .font(.subheadline)
-                .padding(.horizontal, 10)
-
-                List {
-                    if showPortfolio {
-                        ForEach(viewModel.allCoins) { coin in
-                            CoinRow(coin: coin, showHoldings: true)
-                                .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-                        }
-
-                        Text("Lorem ")
-
-                            .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-
-                        Text("Lorem ")
-                            .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-
-                        Text("Lorem ")
-                            .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-
-                    } else {
-                        ForEach(viewModel.portfolioCoins) { coin in
-                            CoinRow(coin: coin, showHoldings: false)
-                                .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-                        }
-                    }
-                }
-                .listStyle(.inset)
-                .transition(.move(edge: .bottom))
-
-                Spacer()
+               Text("Pirce")
             }
-        }
-    }
+            .foregroundStyle(Color.crp.secondaryText)
+            .font(.subheadline)
+            .padding(.horizontal, 10)
+
+            List {
+               if showPortfolio {
+                  ForEach(viewModel.allCoins) { coin in
+                     CoinRow(coin: coin, showHoldings: true)
+                        .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+                  }
+
+                  Text("Lorem ")
+
+                     .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+
+                  Text("Lorem ")
+                     .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+
+               } else {
+                  ForEach(viewModel.portfolioCoins) { coin in
+                     CoinRow(coin: coin, showHoldings: false)
+                        .listRowInsets(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
+                  }
+               }
+            }
+            .listStyle(.inset)
+            .transition(.move(edge: .bottom))
+
+            Spacer()
+         }
+      }
+   }
 }
 
 #Preview {
-    HomeView()
-        .environmentObject(HomeViewModel())
+   HomeView()
+      .environmentObject(HomeViewModel())
 }
 
 extension HomeView {
-    private var homeHeader: some View {
-        VStack {
-            HStack {
-                CircleButton(iconName: showPortfolio ? "plus" : "info")
-                    .animation(.none)
-                    .background(
-                        CircleButtonAnimation(animate: $showPortfolio)
-                    )
+   private var homeHeader: some View {
+      VStack {
+         HStack {
+            CircleButton(iconName: showPortfolio ? "plus" : "info")
+               .animation(.none)
+               .background(
+                  CircleButtonAnimation(animate: $showPortfolio)
+               )
 
-                Spacer()
+            Spacer()
 
-                Text(showPortfolio ? "Portfolio" : "Live Prices")
-                    .foregroundStyle(Color.crp.accent)
-                    .font(.headline)
-                    .animation(.none)
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+               .foregroundStyle(Color.crp.accent)
+               .font(.headline)
+               .animation(.none)
 
-                Spacer()
+            Spacer()
 
-                CircleButton(
-                    iconName: "chevron.right",
-                    action: { withAnimation(.easeOut(duration: 0.5).delay(0)) { showPortfolio.toggle() } }
-                )
-                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
-            }
-        }
-    }
+            CircleButton(
+               iconName: "chevron.right",
+               action: { withAnimation(.easeOut(duration: 0.5).delay(0)) { showPortfolio.toggle() } }
+            )
+            .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+         }
+      }
+   }
 }
